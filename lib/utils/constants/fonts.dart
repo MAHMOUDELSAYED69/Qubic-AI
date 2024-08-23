7b50670b96bc207107996ebeb64d20cc4c163382
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-08-23 12:26:31 +0300

    create EnvManager and FontFamilyManager

---
+++ lib/utils/constants/fonts.dart
@@ -0,0 +1,7 @@
+abstract class FontFamilyManager {
+  const FontFamilyManager._();
+  static const String openSans = "Open Sans";
+  static const String ourceSansPro = "Source Sans Pro";
+  static const String roboto = "Roboto";
+  static const String inter = "Inter";
+}
