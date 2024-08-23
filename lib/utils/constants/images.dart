e6c56ba3eb576df8f00eb56bfeded0ba2599bafc
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-08-23 12:25:45 +0300

    create ImageManager and route manager

---
+++ lib/utils/constants/images.dart
@@ -0,0 +1,5 @@
+abstract class ImageManager {
+  static const String history = "assets/images/history.png";
+  static const String logo = "assets/images/logo.png";
+  static const String chat = "assets/images/chat.png";
+}
---
+++ lib/utils/constants/routes.dart
@@ -0,0 +1,7 @@
+abstract class RouteManager {
+  const RouteManager._();
+  static const String initialRoute = "/";
+  static const String home = "/home";
+  static const String chat = "/chat";
+  static const String settings = "/settings";
+}
